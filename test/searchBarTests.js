87f39f9fe856f073a5a4a5a326e5e30d873c103f
Dimitrios Dimitriadis <melkorinos@gmail.com>
2021-10-24 16:18:48 +0200

    Improve getText solution
    Manage to getText from promise

--- test/searchBarTests.js
+++ test/searchBarTests.js
@@ -17,23 +17,37 @@ describe("Assignment : Search field tests", async function () {
   })
 
   //it block
-  it("Ensures search bar is empty", async function () {
+  it("Ensures special characters are handled from the server", async function () {
 
-    //get the search box text
-    let searchBoxText = await driver.findElement(By.id("site-search-input")).getText();
+    var search_query = `[\`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/`;
 
-    //chai should assert
-    expect(searchBoxText).to.be.empty;
+    //send keys to the search box
+    await driver.findElement(By.id("site-search-input")).sendKeys(search_query);
+
+    //wait till results drop down appears
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="site-search-menu"]/div[3]/a'),10000));
+
+    //find and click more results button
+    await driver.findElement(By.xpath('//*[@id="site-search-menu"]/div[3]/a')).click();
+
+    //wait till next page loads
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="__next"]/div[2]/div[1]/h1'),10000));
+
+    //ensure the next page has results
+    var results = await driver.findElement(By.className('css-11rlpdz eh8fd905'));
+
+    //chai assert
+    expect(results).to.exist;
 
   });
 
   //it block
   it("Checks search query is included in test results", async function () {
 
-    const SEARCH_QUERY = "Band";
+    var search_query = "Band";
 
     //send keys to the search box
-    await driver.findElement(By.id("site-search-input")).sendKeys(SEARCH_QUERY);
+    await driver.findElement(By.id("site-search-input")).sendKeys(search_query);
 
     //wait till results drop down appears
     await driver.wait(until.elementLocated(By.xpath('//*[@id="site-search-menu"]/div[3]/a'),10000));
@@ -43,29 +57,17 @@ describe("Assignment : Search field tests", async function () {
 
     //wait till next page loads
     await driver.wait(until.elementLocated(By.xpath('//*[@id="__next"]/div[2]/div[1]/h3'),10000));
-    
-    //collect the search result elements in an array
-    var elements = [];
-    elements = await driver.findElements(By.className('css-11rlpdz eh8fd905')) 
-    
-    // Iterate over their xpath element for the amount of elements found and assert their text element
-    for (var index=1; index<=elements.length; index++){
-      let text = await driver.findElement(By.xpath(`//*[@id="__next"]/div[2]/div[3]/div[${index}]/a/div/div/div/div/h4`)).getText();
-      expect(text).to.include(SEARCH_QUERY);
-  }
 
-    //Failed attempt to get text element from promises.
-    /*elements = await driver.findElements(By.className('css-11rlpdz eh8fd905')) .then((elements)=>{
-        elements.map((el) => {
-            console.log(el.getAttribute("innerHTML"));              //TRIED   el.getAttribute('h4'  / "textContent" / "innerText"  / "innerHTML")   , el.getText()  
-            el.getAttribute("textContent")((text) =>{
-              
-              expect(text).to.include('123');
-              //console.log(text);
-            });
-        });
-    }); */
+    //collect all the search result elements and get their text
+    let elements = await driver.findElements(By.className('css-11rlpdz eh8fd905'))
+    for (let element of elements) {
+      const text = await element.getText();
+      //assert the text contains the search query
+      expect(text).to.include(search_query);
+    }
     
   });
   
+  //it block
+
 });  
